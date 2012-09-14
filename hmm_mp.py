b0e2c35e05d5a5b8091c046243e8066783faed12
choge <choge.2nd@gmail.com>
2012-09-14 13:31:55 +0900

    added multi-process version (but not implemented yet)

---
+++ hmm_mp.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+import numpy as np
+import ghmm
+import multiprocessing
+import hmm
+
+class MultiProcessHMM(hmm.HMM):
+    """Implementation of HMM with multiprocessing.
+
+    Using multiprocessing module to fasten calculation in estimation
+    step."""
+    pass
+
+
