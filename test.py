fae6020738f0a676d3fabea8f70fafa4a8a1375d
choge <choge.2nd@gmail.com>
2012-09-20 17:15:26 +0900

    test script

---
+++ test.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+import hmm_mp
+
+t = np.array([[0.5, 0.3, 0.2], [0.3, 0.3, 0.4], [0.2, 0.6, 0.2]])
+
+e = np.array([[0.1, 0.2, 0.3], [0.5, 0.2, 0.1], [0.3, 0.1, 0.4], [0.1, 0.5, 0.2]])
+
+init = np.array([0.3, 0.5, 0.2])
+
+def gen_data(num, length, maximum):
+    """Generate dataset as a list of integers."""
+    return np.array([[np.random.randint(0, maximum)
+                     for i in xrange(length)]
+                  for n in xrange(num)])
+
+data = gen_data(10, 50, 4)
+
+h = hmm_mp.MultiProcessHMM(t, e, init)
+h.baum_welch(data, threshold=-1)
