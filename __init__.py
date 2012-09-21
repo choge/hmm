2681efc2f0aaceb89b7ff6cff0867ea9a7fedca2
choge <choge.2nd@gmail.com>
2012-09-21 18:05:39 +0900

    added documentation to __init__.py

--- __init__.py
+++ __init__.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
+"""hmm  An implementation of hidden Markov models in python with numpy.
+
+This is a naïve implementation of hidden Markov models, along with algorithms
+such as Baum-Welch and Viterbi."""
 
 # do nothing
