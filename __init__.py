f91bea5b338521f54c4c779d3de6ae9641b7560f
choge <choge.2nd@gmail.com>
2012-09-21 18:38:01 +0900

    more documentation

--- __init__.py
+++ __init__.py
@@ -3,6 +3,8 @@
 """hmm  An implementation of hidden Markov models in python with numpy.
 
 This is a naïve implementation of hidden Markov models, along with algorithms
-such as Baum-Welch and Viterbi."""
+such as Baum-Welch and Viterbi.
+This module also offers multi-processing calculation, which can boost the
+calculation."""
 
 # do nothing
